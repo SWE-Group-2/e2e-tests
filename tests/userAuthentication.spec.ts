88e43cc704440e86ab19e10298f3883cd8417cd3
Praewvanit Yongvongphaiboon <py@Praewvanits-MacBook-Pro.local>
2024-04-07 03:29:13 +0700

    test user can log out

--- tests/userAuthentication.spec.ts
+++ tests/userAuthentication.spec.ts
@@ -30,4 +30,34 @@ test.describe('User Authentication', () => {
 
         await expect(page).toHaveURL('/internships');
     });
+
+    test('User can logout', async ({page}) => {
+        await page.goto('/login');
+        await expect(page).toHaveURL('/login');
+
+        await expect(page.locator(XPath.getElement('h1'))).toHaveText('LOGIN');
+        await page.locator(XPath.getElementById('username')).fill('admin');
+        await page.locator(XPath.getElementById('password')).fill('hardpass');
+        await page.locator(XPath.getElementByType('submit')).click();
+
+        // navigate to student list via nav bar
+        await expect(page).toHaveURL(/internships/);
+        const nav_account_loc = '//*[@class="dropdown" and contains(text(), "Account")]';
+        const nav_logout_loc = '//*[@class="dropdown-item" and .=" Logout "]';
+        await expect(page.locator(nav_logout_loc)).toBeHidden();
+        await page.locator(nav_account_loc).hover();
+        await expect(page.locator(nav_logout_loc)).toBeVisible();
+        await page.locator(nav_logout_loc).click();
+        await expect(page).toHaveURL(/login/);
+
+        // now the user can't access restricted pages
+        await page.goto('/internships');
+        await page.getByRole('button', { name: 'new' }).click()
+        await expect(page).toHaveURL(/login/);
+
+        await page.goto('/');
+        const nav_student_loc = '//*[@class="navbar-menu"]//*[contains(text(), "Students")]';
+        await page.locator(nav_student_loc).click();
+        await expect(page).toHaveURL(/login/);
+    });
 });
